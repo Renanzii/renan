bc917977a227aece347b368431cfdbaac8a5ddd6
Renanzii <142273880+Renanzii@users.noreply.github.com>
2023-08-28 21:23:50 -0300

    finalizacao lista loops

---
+++ atv7.py.py
@@ -0,0 +1,12 @@
+num = int(input("digite o numero: "))
+auciliar = num
+i = num
+
+
+while i >  1:
+    i -= 1
+
+    num = num * i    
+
+    print(f" {num}")
+
---
+++ atv8.py.py
@@ -0,0 +1,12 @@
+
+for x in range(1,50):
+
+    for y in range(2,x):
+        if x % y == 0:
+            break
+        
+        
+    else:
+
+      print(x)
+             
